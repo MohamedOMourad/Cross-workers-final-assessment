8adec120d9c0c16a3be82c71d18cf4c3783e2bc5
Mohamed Mourad <mohamed.o.murad@gmail.com>
2024-08-10 03:23:11 +0300

    chore: Add @headlessui/react dependency and update App component

--- ui/App.tsx
+++ ui/App.tsx
@@ -1,11 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { listAllEvents } from '/server/actions/events';
+import Layout from './layouts/Layout';
 
 const getAllEvents = async () => {
   return Meteor.callAsync('listAllEvents') as ReturnType<typeof listAllEvents>;
 };
 
 export const App = () => {
-  return <>dasfsd</>;
+  return <Layout />;
 };

4a073669fd1301d8fd39207028fbd698c91ecd15
MohamedOMurad <mohamed.mourad@nagwa.com>
2024-08-11 16:04:44 +0300

    chore: Refactor entities and add Community entity

---
+++ infra/entities/Community.ts
@@ -0,0 +1,3 @@
+type Community = {
+  name: string;
+};
--- server/jobs/CheckInTimeout.ts
+++ server/jobs/CheckInTimeout.ts
@@ -7,7 +7,6 @@ export const RunCheckoutHandlerJob = () => {
     '*/5 * * * * *',
     Meteor.bindEnvironment(async function () {
       const now = new Date();
-      // Find all check-ins where the 5-second window has passed
       const people = await People.updateAsync(
         {
           checkInTime: { $lte: new Date(now.getTime() - 5000) },
@@ -16,7 +15,7 @@ export const RunCheckoutHandlerJob = () => {
         {
           $set: { readyForCheckout: true },
         },
-        { multi: true } // Update multiple documents
+        { multi: true }
       );
       console.log({ people });
     })
